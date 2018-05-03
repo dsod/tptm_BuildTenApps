9022bc1216f59c50a4de207dcf3f47b25b7e3731
dsod <d.soderling@live.se>
2018-05-03 08:01:35 +0200

    Started on app 7. Working with classes!

---
+++ 7_WizardBattle/actors.py
@@ -0,0 +1,10 @@
+
+
+class Wizard:
+    pass
+
+
+class Creature:
+    def __init__(self, name, level):
+        self.name = name
+        self.level = level
---
+++ 7_WizardBattle/program.py
@@ -0,0 +1,46 @@
+import actors
+from actors import Wizard, Creature
+
+
+def main():
+    print_header()
+    game_loop()
+
+
+def print_header():
+    print('-----------------------------')
+    print('      WIZARD GAME APP')
+    print('-----------------------------')
+    print('')
+
+
+def game_loop():
+
+    creatures = [
+        Creature(),
+        Creature(),
+        Creature(),
+        Creature(),
+        Creature(),
+    ]
+
+    hero = Wizard()
+
+
+
+    while True:
+        cmd = input('Do you [a]ttack, [r]unaway, or [l]ook around?')
+        cmd = cmd.lower()
+        if cmd == 'a':
+            print('attack')
+        elif cmd == 'r':
+            print('runaway')
+        elif cmd == 'l':
+            print('lookaround')
+        else:
+            print('OK, exiting game... bye!')
+            break
+
+
+if __name__ == '__main__':
+    main()
