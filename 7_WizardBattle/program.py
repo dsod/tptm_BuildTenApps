0e657feee1629d123e7945d3a27a9a807294f029
dsode <M4dskills>
2018-05-29 23:41:23 +0200

    App 7 completed. A game built on classes.

--- 7_WizardBattle/program.py
+++ 7_WizardBattle/program.py
@@ -1,5 +1,6 @@
-import actors
-from actors import Wizard, Creature
+import random
+import time
+from actors import Wizard, Creature, SmallAnimal, Dragon
 
 
 def main():
@@ -17,30 +18,45 @@ def print_header():
 def game_loop():
 
     creatures = [
-        Creature(),
-        Creature(),
-        Creature(),
-        Creature(),
-        Creature(),
+        SmallAnimal('Toad', 1),
+        Creature('Fox', 5),
+        SmallAnimal('Deadly Snake', 12),
+        Dragon('Dragon', 100, 25, True),
+        Wizard('Evil Wizard', 500),
     ]
 
-    hero = Wizard()
-
+    hero = Wizard('Gandolf', 75)
 
 
     while True:
+
+        active_creature = random.choice(creatures)
+        print('A {} of Level {} has appeared out of a dark and foggy forest...'.format(
+            active_creature.name,
+            active_creature.level
+        ))
+
         cmd = input('Do you [a]ttack, [r]unaway, or [l]ook around?')
         cmd = cmd.lower()
         if cmd == 'a':
-            print('attack')
+            if hero.attack(active_creature):
+                creatures.remove(active_creature)
+            else:
+                print("The Wizard runs and take time to recover.")
+                time.sleep(5)
+                print("The Wizard returns revitalized")
         elif cmd == 'r':
-            print('runaway')
+            print('The Wizard {} has become unsure of his powers and flees.'.format(hero.name))
         elif cmd == 'l':
-            print('lookaround')
+            for c in creatures:
+                print("The wizard {} look around and a {} of level {}.".format(hero.name, c.name, c.level))
         else:
             print('OK, exiting game... bye!')
             break
-
+        if not creatures:
+            print('You have defeated all the creatures!!')
+            break
+        print()
 
 if __name__ == '__main__':
     main()
